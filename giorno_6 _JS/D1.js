1c6bb6e9c0a506c99c170a6b8883e9592ec072b7
Matteo Nardin <nardin.matteo@gmail.com>
2023-10-30 14:42:11 +0100

    js

---
+++ giorno_6 _JS/D1.js
@@ -0,0 +1,71 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per visualizzare l'output, lancia il file HTML a cui è collegato e apri la console dagli strumenti di sviluppo del browser. 
+- Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+console.log("i tipi di javascript sono: string (tutti i caratteri della tastiera), number (tutti in numeri), bool (sono i valori true e false), null(valore nullo), undefined(valore non assegnato)");
+
+/* ESERCIZIO 2
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+let name = "Marco";
+console.log(name);
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+let somma = 12 + 20;
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+let x = 12;
+
+/* ESERCIZIO 5
+  Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
+  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
+*/
+
+name = "Rossi";
+console.log("riassegno un valore a name: ", name);
+
+const valore = "pippo";
+// non posso riassegnare 'valore' perché è const
+// valore = "pluto"; 
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+let sottrazione = 4 - x;
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+*/
+
+let name1 = "john";
+let name2 = "John";
+
+if (name1 === name2){
+  console.log("I nomi sono uguali");
+}else{
+  console.log("i nomi son diversi");
+};
+
+if (name1.toLocaleLowerCase === name2.toLocaleLowerCase){
+  console.log("I nomi sono uguali");
+}else{
+  console.log("i nomi son diversi");
+};
