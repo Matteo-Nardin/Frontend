043bac599cf18f403079c5a4bfc1a52f67c3dd12
Matteo Nardin <nardin.matteo@gmail.com>
2023-11-21 17:30:21 +0100

    mediaquery

--- giorno_13_js/js/script.js
+++ giorno_13_js/js/script.js
@@ -1,16 +1,15 @@
 // crea programmaticamente, utilizzando JS, un tabellone per la tombola formato da 76 celle, numerate con i numeri da 1 a  76
 let tabella = document.querySelector("#tabella");
 // let cella = document.createElement("div");
+const arr =[0]; // soluzione barbona
 const button = document.querySelector("#extract");
 for(let i=1; i <=76; i++){
-    
     let cella = document.createElement("div");
-    cella.className = `cel_${[i]}`;
+    cella.className = `cel`;
     cella.innerText = i;
     tabella.appendChild(cella);
-
     
-  
+    arr.push(i);
 }
 console.log(tabella.childNodes[1].innerText)
 console.log(typeof tabella.childNodes[1])
@@ -21,14 +20,31 @@ console.log(typeof tabella.childNodes[1])
 // Crea un bottone per simulare l'estrazione di un numero: dovrà selezionare un numero da 1 a 76
 
 // dopo il click del bottone, evidenzia in qualche modo la cella corrispondente sul tabellone (quello con lo stesso numero) , ad esempio cambiandone il colore di sfondo
+// btn.addEventListener("click", ()=> nomefunzione(num)) così posso mettere una funzione con parametro
 
 button.addEventListener("click", function(){
-    let rand = Math.round(Math.random()*76);
-    tabella.childNodes.forEach(x => x.innerText == rand ? x.style.backgroundColor = "#ddd" : console.log(rand));
-    return rand;
-});
-console.log(rand)
+    let rand = Math.round(Math.random()*75);
+    // tabella.childNodes.forEach(x => x.innerText == rand ? x.style.backgroundColor = "#ddd" : null);
+    console.log(arr);
 
+    let exist = arr.find((x) => x === rand );
+    while(){
+        
+    }
+    if(exist){
+        console.log(rand, "c'è già");
+    }else{
+
+        tabella.childNodes.forEach( x => {
+           if(x.innerText == rand){
+                x.style.backgroundColor = "#ddd";
+            } 
+        });
+    
+        arr.push(rand);
+    }
+
+});
 
 // mantieni evidezniate le celle corrispondenti ai numeri precedentemente estratti dopo ogni estrazione
 
